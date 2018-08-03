e9138c9b3496e554f55f6c7f9576c9e85f2856a8
kanon <vipkia@sina.cn>
2018-08-03 13:59:06 +0800

    完成重置密码前端页面

--- src/components/PasswordChange.vue
+++ src/components/PasswordChange.vue
@@ -1,14 +1,11 @@
 <template>
     <div id="password_change">
-        <Row>
-            <Col span="4" offset="10">
+        <Row v-if="!showMessage" v-show="!showResult">
+            <Col span="6" offset="9">
                 <h2>请设置{{backEmail}}的新密码，建议使用数字，字母，字符组合密码，提高密码安全性</h2>
                 <Form ref="formItems" :model="formItems" :rules="ruleValidate">
                     <FormItem prop="username">
                         <Input v-model="formItems.username" placeholder="用户名" :autofocus='true'></Input>
-                    </FormItem>
-                    <FormItem prop="email">
-                        <Input type="email" v-model="formItems.email" placeholder="邮箱"></Input>
                     </FormItem>
                     <FormItem prop="password">
                         <Input type="password" v-model="formItems.password" placeholder="新密码"></Input>
@@ -22,18 +19,32 @@
                 </Form>
             </Col>
         </Row>
+        <Row v-if="showMessage" v-show="!showResult">
+            <Col span="8" offset="8">
+                <h1 class="title is-4">链接以失效，请重新发送邮件重置密码！</h1>
+            </Col>
+        </Row>
+        <Row v-if="showResult">
+            <Col span="6" offset="9">
+                <h1 class="title is-4">密码重置成功！</h1>
+                <a href="/">点击链接，跳转到登录页面</a>
+            </Col>
+        </Row>
     </div>    
 </template>
 <script>
+var base64 = require('js-base64').Base64
+import axios from 'axios'
+const qs = require('qs')
 export default {
     data() {
         return {
-            backEmail: '**est@qq.com',
+            showResult: false,
+            showMessage: false,
+            backEmail: '',
             backUsername: '',
-            timestamp: '',
             formItems: {
                 username: '',
-                email: '',
                 password: '',
                 repeatPassword: ''
             },
@@ -41,12 +52,9 @@ export default {
                 username: [
                     { required: true, message: '用户名不能为空', trigger: 'blur' }
                 ],
-                email: [
-                    { required: true, message: '邮箱不能为空', trigger: 'blur' },
-                    { type: 'email', message: '不符合邮箱规范', trigger: 'blur' }
-                ],
                 password: [
-                    { required: true, message: '新密码不能为空', trigger: 'blur' }
+                    { required: true, message: '新密码不能为空', trigger: 'blur' },
+                    { type: 'string', min: 6, message: '新密码不少于6位', trigger: 'blur' }
                 ],
                 repeatPassword: [
                     { required: true, message: '重新输入密码不能为空', trigger: 'blur' }
@@ -65,7 +73,7 @@ export default {
                         });
                         return;
                     }
-                    if (this.formItems.username !== this.backEmail) {
+                    if (this.formItems.username !== this.backUsername) {
                         this.$Message.error({
                             content: '该用户不存在！',
                             duration: 2
@@ -98,12 +106,14 @@ export default {
                             switch(data.code) {
                                 //请求发送邮件成功
                                 case 200:
-                                    break;
-                                //用户信息校验失败
-                                case 400:
+                                    _this.showResult = true;
                                     break;
                                 //用户信息校验成功，邮件发送失败
                                 default:
+                                    _this.$Message.error({
+                                        content: data.msg,
+                                        duration: 3
+                                    });
                             }
                         }],
                     });
@@ -113,18 +123,24 @@ export default {
     },
     mounted() {
         let params = this.$route.query.sid;
+        params = base64.decode(params);
+        let array = params.split('&');
+        this.backUsername = array[0].split('=')[1];
+        this.backEmail = array[1].split('=')[1];
+        let sys_timstamp = parseInt(parseInt(array[2].split('=')[1]) / 1000);
+        // console.log(array)
+        let username = params;
         let time = Date.parse(new Date()) /1000;
-        if (this.timestamp + 86400 < time) {
+        if (sys_timstamp + 30000 < time) {
             //超时，所有输入框和按钮都为disabled
-        }
-        console.log(params);
-
+            this.showMessage = true;
+        } 
     }
 }
 </script>
 <style scoped>
 #password_change {
-    padding-top: 13%
+    padding-top: 9%
 }
 </style>
 
--- src/components/PasswordFind.vue
+++ src/components/PasswordFind.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="find_password">
-        <Row>
+        <Row v-if="!showResult">
             <Col span="4" offset="10">
                 <h2 class="title is-4">密码找回</h2>
                 <Form ref="formItems" :model="formItems" :rules="ruleValidate">
@@ -16,12 +16,21 @@
                 </Form>
             </Col>
         </Row>
+        <Row v-if="showResult">
+            <Col span="8" offset="8">
+                <h1 class="title is-4">邮件发送成功，请立即前往收件箱点击相关链接，重置密码！</h1>
+                <a href="http://127.0.0.1:8080/sysadmin/password_find">如未受到邮件，请点击这里重新发送邮件！>></a>
+            </Col>
+        </Row>
     </div>
 </template>
 <script>
+import axios from 'axios'
+const qs = require('qs')
 export default {
     data() {
         return {
+            showResult: false,
             formItems: {
                 username: '',
                 email: ''
@@ -44,6 +53,7 @@ export default {
                     let params = this.formItems;
                     let _this = this;
                     _this.$Spin.show();
+                    console.log(params)
                     axios({
                         url: 'http://127.0.0.1:5000/sysadmin/verify',
                         method: 'post',
@@ -64,15 +74,25 @@ export default {
                             // Do whatever you want to transform the data
                             _this.$Spin.hide();
                             data = JSON.parse(data);
+                            console.log(data)
                             switch(data.code) {
                                 //请求发送邮件成功
                                 case 200:
+                                    _this.showResult = true;
                                     break;
                                 //用户信息校验失败
                                 case 400:
+                                    _this.$Message.error({
+                                        content: '用户信息不正确，请确认信息正确性！',
+                                        duration: 3
+                                    });
                                     break;
                                 //用户信息校验成功，邮件发送失败
                                 default:
+                                    _this.$Message.error({
+                                        content: '邮件发送失败，可能邮件系统出现问题，请联系管理员！',
+                                        duration: 3
+                                    });
                             }
                         }],
                     });
